a9f9f4ed3aad89ecf8ca840f0ae6de29e108a54b
Moons <me@moons.bio>
2024-09-10 01:05:19 +0200

    deprecate: staff requirement no longer needed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,8 +5,6 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center py-2">
       <h1 className="text-4xl font-bold mb-8">Please log in with your PFQ account.</h1>
       <LoginPanel Destination="/pokedex" />
-      <br />
-      <p className="text-sm text-gray-500"><i>Note: This will only work with Staff accounts.</i></p>
     </div>
   )
 }
