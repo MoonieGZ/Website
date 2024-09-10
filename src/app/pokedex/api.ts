229b18f7ad41e31c53480b5476e8cb0a0dc9be31
Moons <me@moons.bio>
2024-09-10 03:22:43 +0200

    fix: natures filtering

--- src/app/pokedex/api.ts
+++ src/app/pokedex/api.ts
@@ -22,8 +22,6 @@ export const fetchOwnedPokemon = async (token: string, settings: UserSettings): 
   const url = new URL("https://api.pokefarm.com/pokemon")
   
   if (settings.natures.length > 0) url.searchParams.append("natures", settings.natures.join(","))
-  if (settings.berryPreferences.length > 0) url.searchParams.append("berries", settings.berryPreferences.join(","))
-  if (settings.pokemonTypes.length > 0) url.searchParams.append("types", settings.pokemonTypes.join(","))
   
   const response = await fetch(url.toString(), {
     headers: {
